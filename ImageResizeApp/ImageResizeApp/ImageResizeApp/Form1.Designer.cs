340617de5263a4767963a98d3e4847bcf2bc4822
Ali Muradov <amuradov239@gmail.com>
2024-03-06 16:14:51 +0200

    Finished app creation

---
+++ ImageResizeApp/ImageResizeApp/ImageResizeApp/Form1.Designer.cs
@@ -0,0 +1,159 @@
+﻿namespace ImageResizeApp
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private new void InitializeComponent()
+        {
+            this.OriginalPictureBox = new System.Windows.Forms.PictureBox();
+            this.SequentialPictureBox = new System.Windows.Forms.PictureBox();
+            this.ParallelPictureBox = new System.Windows.Forms.PictureBox();
+            this.DownscaleFactorTextBox = new System.Windows.Forms.TextBox();
+            this.OpenButton = new System.Windows.Forms.Button();
+            this.DownscaleButton = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.BetterQualityPictureBox = new System.Windows.Forms.PictureBox();
+            this.BetterQualityLabel = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.OriginalPictureBox)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.SequentialPictureBox)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.ParallelPictureBox)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.BetterQualityPictureBox)).BeginInit();
+            this.SuspendLayout();
+            
+            this.OriginalPictureBox.Location = new System.Drawing.Point(52, 12);
+            this.OriginalPictureBox.Name = "OriginalPictureBox";
+            this.OriginalPictureBox.Size = new System.Drawing.Size(200, 200);
+            this.OriginalPictureBox.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.OriginalPictureBox.TabIndex = 0;
+            this.OriginalPictureBox.TabStop = false;
+            
+            this.SequentialPictureBox.Location = new System.Drawing.Point(379, 12);
+            this.SequentialPictureBox.Name = "SequentialPictureBox";
+            this.SequentialPictureBox.Size = new System.Drawing.Size(200, 200);
+            this.SequentialPictureBox.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.SequentialPictureBox.TabIndex = 1;
+            this.SequentialPictureBox.TabStop = false;
+            
+            this.ParallelPictureBox.Location = new System.Drawing.Point(694, 12);
+            this.ParallelPictureBox.Name = "ParallelPictureBox";
+            this.ParallelPictureBox.Size = new System.Drawing.Size(200, 200);
+            this.ParallelPictureBox.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.ParallelPictureBox.TabIndex = 2;
+            this.ParallelPictureBox.TabStop = false;
+            
+            this.DownscaleFactorTextBox.Location = new System.Drawing.Point(337, 417);
+            this.DownscaleFactorTextBox.Name = "DownscaleFactorTextBox";
+            this.DownscaleFactorTextBox.Size = new System.Drawing.Size(190, 26);
+            this.DownscaleFactorTextBox.TabIndex = 3;
+            this.DownscaleFactorTextBox.Text = "Downscale size (%)";
+           
+            this.OpenButton.Location = new System.Drawing.Point(122, 468);
+            this.OpenButton.Name = "OpenButton";
+            this.OpenButton.Size = new System.Drawing.Size(166, 42);
+            this.OpenButton.TabIndex = 4;
+            this.OpenButton.Text = "Open Image";
+            this.OpenButton.UseVisualStyleBackColor = true;
+            this.OpenButton.Click += new System.EventHandler(this.OpenButton_Click);
+            
+            this.DownscaleButton.Location = new System.Drawing.Point(337, 468);
+            this.DownscaleButton.Name = "DownscaleButton";
+            this.DownscaleButton.Size = new System.Drawing.Size(189, 42);
+            this.DownscaleButton.TabIndex = 5;
+            this.DownscaleButton.Text = "Downscale";
+            this.DownscaleButton.UseVisualStyleBackColor = true;
+            this.DownscaleButton.Click += new System.EventHandler(this.DownscaleButton_Click);
+            
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(68, 236);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(111, 20);
+            this.label1.TabIndex = 6;
+            this.label1.Text = "Original Image";
+            
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(398, 236);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(160, 20);
+            this.label2.TabIndex = 7;
+            this.label2.Text = "Consequential Image";
+            
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(745, 236);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(109, 20);
+            this.label3.TabIndex = 8;
+            this.label3.Text = "Parallel Image";
+            
+            this.BetterQualityPictureBox.Location = new System.Drawing.Point(1006, 12);
+            this.BetterQualityPictureBox.Name = "BetterQualityPictureBox";
+            this.BetterQualityPictureBox.Size = new System.Drawing.Size(200, 200);
+            this.BetterQualityPictureBox.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.BetterQualityPictureBox.TabIndex = 11;
+            this.BetterQualityPictureBox.TabStop = false;
+            
+            this.BetterQualityLabel.AutoSize = true;
+            this.BetterQualityLabel.Location = new System.Drawing.Point(1002, 236);
+            this.BetterQualityLabel.Name = "BetterQualityLabel";
+            this.BetterQualityLabel.Size = new System.Drawing.Size(356, 20);
+            this.BetterQualityLabel.TabIndex = 12;
+            this.BetterQualityLabel.Text = "Image without quality improvement (For Example)";
+           
+            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1724, 705);
+            this.Controls.Add(this.BetterQualityPictureBox);
+            this.Controls.Add(this.BetterQualityLabel);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.DownscaleButton);
+            this.Controls.Add(this.OpenButton);
+            this.Controls.Add(this.DownscaleFactorTextBox);
+            this.Controls.Add(this.ParallelPictureBox);
+            this.Controls.Add(this.SequentialPictureBox);
+            this.Controls.Add(this.OriginalPictureBox);
+            this.Name = "Form1";
+            this.Text = "Image Resize App";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.OriginalPictureBox)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.SequentialPictureBox)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.ParallelPictureBox)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.BetterQualityPictureBox)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+    }
+}
---
+++ ImageResizeApp/ImageResizeApp/ImageResizeApp/Form1.cs
@@ -0,0 +1,352 @@
+﻿using System;
+using System.Drawing;
+using System.Diagnostics;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace ImageResizeApp
+{
+    public partial class Form1 : Form
+    {
+        private Bitmap originalImage;
+
+        private PictureBox OriginalPictureBox;
+        private PictureBox SequentialPictureBox;
+        private PictureBox ParallelPictureBox;
+        private TextBox DownscaleFactorTextBox;
+        private Button OpenButton;
+        private Button DownscaleButton;
+        private Label OriginalLabel;
+        private Label SequentialLabel;
+        private Label ParallelLabel;
+        private PictureBox BetterQualityPictureBox;
+        private Label BetterQualityLabel;
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            OriginalLabel = new Label();
+            SequentialLabel = new Label();
+            ParallelLabel = new Label();
+            BetterQualityLabel = new Label();
+
+            OriginalLabel.AutoSize = true;
+            OriginalLabel.Location = new Point(12, 215);
+            OriginalLabel.Name = "OriginalLabel";
+            OriginalLabel.TabIndex = 6;
+
+            SequentialLabel.AutoSize = true;
+            SequentialLabel.Location = new Point(250, 215);
+            SequentialLabel.Name = "SequentialLabel";
+            SequentialLabel.TabIndex = 7;
+
+            ParallelLabel.AutoSize = true;
+            ParallelLabel.Location = new Point(488, 215);
+            ParallelLabel.Name = "ParallelLabel";
+            ParallelLabel.TabIndex = 8;
+
+            BetterQualityLabel.AutoSize = true; 
+            BetterQualityLabel.Location = new Point(999, 215); 
+            BetterQualityLabel.Name = "BetterQualityLabel"; 
+            BetterQualityLabel.TabIndex = 9; 
+            
+            Controls.Add(OriginalLabel);
+            Controls.Add(SequentialLabel);
+            Controls.Add(ParallelLabel);
+            Controls.Add(BetterQualityLabel); 
+
+       
+
+            Controls.Add(BetterQualityPictureBox);
+            Controls.Add(BetterQualityLabel); 
+        }
+
+        private void UpdateImageLabels(Bitmap image, Label label)
+        {
+            if (image != null)
+            {
+                label.Text = $"Size: {image.Width} x {image.Height}";
+            }
+            else
+            {
+                label.Text = "Size: -";
+            }
+        }
+
+        private void OpenButton_Click(object sender, EventArgs e)
+        {
+            using (OpenFileDialog openFileDialog = new OpenFileDialog())
+            {
+                openFileDialog.Filter = "Image Files (*.bmp;*.jpg;*.png)|*.bmp;*.jpg;*.png|All files (*.*)|*.*";
+                if (openFileDialog.ShowDialog() == DialogResult.OK)
+                {
+                    try
+                    {
+                        originalImage = new Bitmap(openFileDialog.FileName);
+                        OriginalPictureBox.Image = originalImage;
+                        UpdateImageLabels(originalImage, OriginalLabel);
+                    }
+                    catch (Exception ex)
+                    {
+                        MessageBox.Show($"Error loading image: {ex.Message}", "Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                    }
+                }
+            }
+        }
+
+        private async void DownscaleButton_Click(object sender, EventArgs e)
+        {
+            if (originalImage == null)
+            {
+                MessageBox.Show("Please select an image first.", "Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                return;
+            }
+
+            if (!double.TryParse(DownscaleFactorTextBox.Text, out double downscaleFactor) || downscaleFactor <= 0 || downscaleFactor > 100)
+            {
+                MessageBox.Show("Invalid downscaling factor. Enter a value between 1 and 100.", "Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                return;
+            }
+
+            long sequentialElapsedMilliseconds;
+            long parallelElapsedMilliseconds;
+            long betterQualityElapsedMilliseconds; 
+
+            Bitmap sequentialResult = DownscaleSequential(originalImage, downscaleFactor, out sequentialElapsedMilliseconds);
+            SequentialPictureBox.Image = sequentialResult;
+            UpdateImageLabels(sequentialResult, SequentialLabel);
+
+
+            Tuple<Bitmap, long> parallelResultTuple = await DownscaleParallel(originalImage, downscaleFactor);
+            Bitmap parallelResult = parallelResultTuple.Item1;
+            parallelElapsedMilliseconds = parallelResultTuple.Item2;
+
+
+            Bitmap betterQualityResult = DownscaleBetterQuality(originalImage, downscaleFactor);
+            BetterQualityPictureBox.Image = betterQualityResult; 
+            BetterQualityPictureBox.Refresh(); 
+            UpdateImageLabels(betterQualityResult, BetterQualityLabel); 
+            betterQualityElapsedMilliseconds = CalculateElapsedMilliseconds(() => DownscaleBetterQuality(originalImage, downscaleFactor));
+
+            if (sequentialResult != null && parallelResult != null && betterQualityResult != null)
+            {
+              
+                ParallelPictureBox.Image = parallelResult;
+                UpdateImageLabels(parallelResult, ParallelLabel);
+
+                MessageBox.Show($"Sequential Time: {sequentialElapsedMilliseconds} ms\nParallel Time: {parallelElapsedMilliseconds} ms\nBetter Quality Time: {betterQualityElapsedMilliseconds} ms", "Performance Metrics", MessageBoxButtons.OK, MessageBoxIcon.Information);
+            }
+        }
+
+        private long CalculateElapsedMilliseconds(Action action)
+        {
+            Stopwatch stopwatch = new Stopwatch();
+            stopwatch.Start();
+            action.Invoke();
+            stopwatch.Stop();
+            return stopwatch.ElapsedMilliseconds;
+        }
+
+        private Bitmap DownscaleBetterQuality(Bitmap sourceImage, double downscaleFactor)
+        {
+            Stopwatch stopwatch = new Stopwatch();
+            stopwatch.Start();
+
+            int newWidth = Math.Max(1, (int)(sourceImage.Width * downscaleFactor / 100));
+            int newHeight = Math.Max(1, (int)(sourceImage.Height * downscaleFactor / 100));
+
+            Bitmap resultImage = new Bitmap(newWidth, newHeight);
+
+            for (int x = 0; x < newWidth; x++)
+            {
+                for (int y = 0; y < newHeight; y++)
+                {
+                    Color averagedColor = AveragePixels(sourceImage, x, y, downscaleFactor);
+                    resultImage.SetPixel(x, y, averagedColor);
+                }
+            }
+
+            stopwatch.Stop();
+            long elapsedMilliseconds = stopwatch.ElapsedMilliseconds;
+
+            return resultImage;
+        }
+
+        private Color AveragePixels(Bitmap originalImage, int x, int y, double factor)
+        {
+            int newWidth = (int)(originalImage.Width * factor / 100);
+            int newHeight = (int)(originalImage.Height * factor / 100);
+
+            int startX = (int)(x * originalImage.Width / newWidth);
+            int startY = (int)(y * originalImage.Height / newHeight);
+
+            int endX = Math.Min((int)((x + 1) * originalImage.Width / newWidth), originalImage.Width - 1);
+            int endY = Math.Min((int)((y + 1) * originalImage.Height / newHeight), originalImage.Height - 1);
+
+            int totalRed = 0, totalGreen = 0, totalBlue = 0;
+
+            for (int i = startX; i <= endX; i++)
+            {
+                for (int j = startY; j <= endY; j++)
+                {
+                    Color pixelColor = originalImage.GetPixel(i, j);
+                    totalRed += pixelColor.R;
+                    totalGreen += pixelColor.G;
+                    totalBlue += pixelColor.B;
+                }
+            }
+
+            int pixelCount = (endX - startX + 1) * (endY - startY + 1);
+
+            int averagedRed = totalRed / pixelCount;
+            int averagedGreen = totalGreen / pixelCount;
+            int averagedBlue = totalBlue / pixelCount;
+
+            return Color.FromArgb(averagedRed, averagedGreen, averagedBlue);
+        }
+
+        private Bitmap DownscaleSequential(Bitmap sourceImage, double downscaleFactor, out long elapsedMilliseconds)
+        {
+            Stopwatch stopwatch = new Stopwatch();
+            stopwatch.Start();
+
+            int newWidth = Math.Max(1, (int)(sourceImage.Width * downscaleFactor / 100));
+            int newHeight = Math.Max(1, (int)(sourceImage.Height * downscaleFactor / 100));
+
+            Bitmap resultImage = new Bitmap(newWidth, newHeight);
+
+            for (int x = 0; x < newWidth; x++)
+            {
+                for (int y = 0; y < newHeight; y++)
+                {
+                    Color weightedColor = ComputeWeightedAverageLanczos(sourceImage, x, y, downscaleFactor);
+                    resultImage.SetPixel(x, y, weightedColor);
+                }
+            }
+
+            stopwatch.Stop();
+            elapsedMilliseconds = stopwatch.ElapsedMilliseconds;
+
+            return resultImage;
+        }
+
+        private async Task<Tuple<Bitmap, long>> DownscaleParallel(Bitmap sourceImage, double downscaleFactor)
+        {
+            Stopwatch stopwatch = new Stopwatch();
+            stopwatch.Start();
+
+            int newWidth = Math.Max(1, (int)(sourceImage.Width * downscaleFactor / 100));
+            int newHeight = Math.Max(1, (int)(sourceImage.Height * downscaleFactor / 100));
+
+            if (newWidth <= 0 || newHeight <= 0)
+            {
+                MessageBox.Show("Invalid downscaling factor. Resulting image dimensions are non-positive.", "Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                stopwatch.Stop();
+                return null;
+            }
+
+            Bitmap resultImage = new Bitmap(newWidth, newHeight);
+
+            object lockObject = new object();
+
+            await Task.Run(() =>
+            {
+                Parallel.For(0, newWidth, x =>
+                {
+                    for (int y = 0; y < newHeight; y++)
+                    {
+                        Color weightedColor;
+
+                        lock (lockObject)
+                        {
+                            weightedColor = ComputeWeightedAverageLanczos(sourceImage, x, y, downscaleFactor);
+                        }
+
+                        lock (resultImage)
+                        {
+                            resultImage.SetPixel(x, y, weightedColor);
+                        }
+                    }
+                });
+            });
+
+            stopwatch.Stop();
+            long elapsedMilliseconds = stopwatch.ElapsedMilliseconds;
+
+            ParallelPictureBox.Image = resultImage;
+
+            return new Tuple<Bitmap, long>(resultImage, elapsedMilliseconds);
+        }
+
+        private Color ComputeWeightedAverageLanczos(Bitmap originalImage, int x, int y, double factor)
+        {
+            int newWidth = (int)(originalImage.Width * factor / 100);
+            int newHeight = (int)(originalImage.Height * factor / 100);
+
+            double u = x * (originalImage.Width - 1.0) / (newWidth - 1.0);
+            double v = y * (originalImage.Height - 1.0) / (newHeight - 1.0);
+
+            int startX = (int)Math.Floor(u - 2);
+            int endX = (int)Math.Ceiling(u + 2);
+
+            int startY = (int)Math.Floor(v - 2);
+            int endY = (int)Math.Ceiling(v + 2);
+
+            double totalWeight = 0;
+            double totalRed = 0, totalGreen = 0, totalBlue = 0;
+
+            for (int i = startX; i <= endX; i++)
+            {
+                for (int j = startY; j <= endY; j++)
+                {
+                    double weight = LanczosKernel(u - i) * LanczosKernel(v - j);
+
+                    int px = Math.Min(Math.Max(0, i), originalImage.Width - 1);
+                    int py = Math.Min(Math.Max(0, j), originalImage.Height - 1);
+
+                    Color pixelColor;
+
+                    lock (originalImage)
+                    {
+                        pixelColor = originalImage.GetPixel(px, py);
+                    }
+
+                    totalRed += pixelColor.R * weight;
+                    totalGreen += pixelColor.G * weight;
+                    totalBlue += pixelColor.B * weight;
+                    totalWeight += weight;
+                }
+            }
+
+            int averagedRed = (int)(totalRed / totalWeight);
+            int averagedGreen = (int)(totalGreen / totalWeight);
+            int averagedBlue = (int)(totalBlue / totalWeight);
+
+            averagedRed = (int)Math.Max(0, Math.Min(averagedRed, 255));
+            averagedGreen = (int)Math.Max(0, Math.Min(averagedGreen, 255));
+            averagedBlue = (int)Math.Max(0, Math.Min(averagedBlue, 255));
+
+            return Color.FromArgb(averagedRed, averagedGreen, averagedBlue);
+        }
+
+        private double LanczosKernel(double x)
+        {
+            if (Math.Abs(x) < double.Epsilon)
+            {
+                return 1.0;
+            }
+            else if (Math.Abs(x) < 2.0)
+            {
+                return Math.Sin(Math.PI * x) * Math.Sin(Math.PI * x / 2.0) / (Math.PI * x * Math.PI * x / 2.0);
+            }
+            else
+            {
+                return 0.0;
+            }
+        }
+    }
+}
